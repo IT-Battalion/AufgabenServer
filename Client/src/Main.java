225ee53c283d8b07f68f2b0c6ab0c12d601e32ce
pelias <pelias@student.tgm.ac.at>
2022-05-16 15:38:53 +0200

    Add: Client Sided Socket connection.

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,10 +1,19 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Das ist der Client, welcher sich zum Server verbindet und dann zufällige Fragen
  * erhält.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting Client...");
         String port = args.length == 0 ? "2445" : args[0];
-
+        System.out.println("Port: " + port);
+        ServerSocket server = new ServerSocket(Integer.parseInt(port));
+        System.out.println("Waiting for Server connection...");
+        Socket socket = server.accept();
+        System.out.println("Connection to Server established.");
     }
 }
